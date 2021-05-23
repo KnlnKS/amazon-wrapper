330f6d47a5eaf1afdbf49a4baf3fbe27fb5399d7
KnlnKS <soulkks@hotmail.ca>
2021-05-22 22:40:36 -0400

    🎨 Improve Code Structure

--- item/item.go
+++ item/item.go
@@ -24,35 +24,25 @@ func GetItemPage(item string, variant string) (document *goquery.Document) {
 	return document
 }
 
-func GetItem(item string, variant string) (productTitle string, price string, rating string, numReviews string, landingImage string, hiResLandingImage string) {
+func GetItem(item string, variant string) (res gin.H) {
 	document := GetItemPage(item, variant)
-	var exists bool
 
-	productTitle = strings.TrimSpace(document.Find("#productTitle").Text())
-	price = document.Find("#priceblock_ourprice").Text()
-	rating = document.Find(".a-icon-alt").First().Text()
-	numReviews = document.Find("#acrCustomerReviewText").First().Text()
-	landingImage, exists = document.Find("#landingImage").Attr("src")
+	productTitle := strings.TrimSpace(document.Find("#productTitle").Text())
+	price := document.Find("#priceblock_ourprice").Text()
+	rating := document.Find(".a-icon-alt").First().Text()
+	numReviews := document.Find("#acrCustomerReviewText").First().Text()
+	landingImage, exists := document.Find("#landingImage").Attr("src")
 	if !exists {
 		log.Println("Cannot find Landing Image")
 		landingImage = "N/A"
 	}
-	hiResLandingImage, exists = document.Find("#landingImage").Attr("data-old-hires")
+	hiResLandingImage, exists := document.Find("#landingImage").Attr("data-old-hires")
 	if !exists {
 		log.Println("Cannot find Hi Res Landing Image")
 		hiResLandingImage = "N/A"
 	}
 
-	return productTitle, price, rating, numReviews, landingImage, hiResLandingImage
-}
-
-func Start(c *gin.Context) {
-	item := c.Param("item")
-	variant := c.Param("variant")
-
-	productTitle, price, rating, numReviews, landingImage, hiResLandingImage := GetItem(item, variant)
-
-	c.JSON(200, gin.H{
+	res = gin.H{
 		"url":               "https://www.amazon.ca/" + item + "/dp/" + variant,
 		"productTitle":      productTitle,
 		"price":             price,
@@ -60,5 +50,14 @@ func Start(c *gin.Context) {
 		"rating":            rating,
 		"landingImage":      landingImage,
 		"hiResLandingImage": hiResLandingImage,
-	})
+	}
+
+	return res
+}
+
+func Start(c *gin.Context) {
+	item := c.Param("item")
+	variant := c.Param("variant")
+
+	c.JSON(200, GetItem(item, variant))
 }
